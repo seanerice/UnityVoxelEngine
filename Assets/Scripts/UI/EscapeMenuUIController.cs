18a833ef99c39c3c27f267582033e2c56e663a59
Sean Rice <seane.rice@gmail.com>
2018-06-22 18:46:12 -0500

    Reorganize folder, start adding escape menu.

---
+++ Assets/Scripts/UI/EscapeMenuUIController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeMenuUIController : MonoBehaviour {
+
+	Canvas ThisCanvas;
+	public UnityStandardAssets.Characters.FirstPerson.FirstPersonController FirstPersonController;
+	public Canvas GraphicsCanvas;
+	public Canvas SoundCanvas;
+	public Canvas DebugCanvas;
+
+	// Use this for initialization
+	void Start () {
+		ThisCanvas = gameObject.GetComponent<Canvas>();
+		ThisCanvas.enabled = false;
+		DeactivateAllCanvases();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			ThisCanvas.enabled = !ThisCanvas.isActiveAndEnabled;
+			FirstPersonController.m_MouseLook.UpdateCursorLock();
+			FirstPersonController.enabled = !FirstPersonController.isActiveAndEnabled;
+			DeactivateAllCanvases();
+		}
+	}
+
+	public void DeactivateAllCanvases() {
+		GraphicsCanvas.enabled = false;
+		SoundCanvas.enabled = false;
+		DebugCanvas.enabled = false;
+	}
+
+	public void Graphics() {
+		DeactivateAllCanvases();
+		GraphicsCanvas.enabled = true;
+	}
+
+	public void Sound() {
+		DeactivateAllCanvases();
+		SoundCanvas.enabled = true;
+	}
+
+	public void DebugMenu() {
+		DeactivateAllCanvases();
+		DebugCanvas.enabled = true;
+	}
+
+	public void Quit () {
+		Application.Quit();
+	}
+}
